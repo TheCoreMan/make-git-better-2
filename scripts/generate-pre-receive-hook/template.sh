c83055076a03895104113f4276f5d2bff3cdd457
Shay Nehmad <shay.nehmad@guardicore.com>
2020-05-17 20:38:24 +0300

    debugging the scripts and added the flags

--- scripts/generate-pre-receive-hook/template.sh
+++ scripts/generate-pre-receive-hook/template.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-current_branch=`git rev-parse --abbrev-ref HEAD`
+read old new ref < /dev/stdin
 
-case $current_branch in
+branch_name=$(echo $ref | awk 'BEGIN \{ FS = "/" } ; \{ print $NF }')
+
+case $branch_name in
 {{ for level in levels }}{level.branch})
-    {level.solution_checker}
+    echo $old $new $ref | {level.solution_checker}
+    echo "solved! the flags are..."
+    {{ for levelflag in level.flags }}
+    echo {levelflag}
+    {{ endfor }}
     ;;
 {{ endfor }}esac

b198847022291958aaeef7d6729e8fef3fa5ba7b
Shay Nehmad <shay.nehmad@guardicore.com>
2020-05-19 11:48:08 +0300

    Dockerfile moved to build dir along with helper scripts - still in testing

---
+++ build/gamemaster_entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ ! $(whoami) == "gamemaster" ]] 
+	then echo "I'm not the gamemaster"; exit 1; 
+fi
+
+if [[ ! -f /tmp/id_rsa.player.pub ]]
+	then echo "Not public key file found"; exit 1;
+fi
+
+# https://git-scm.com/book/en/v2/Git-on-the-Server-Setting-Up-the-Server
+cd
+pwd
+mkdir .ssh && chmod 700 .ssh
+cat /tmp/id_rsa.player.pub >> ~/.ssh/authorized_keys
+

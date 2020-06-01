4680d1da6d97d07aac807a230c2d70eeeba10a53
Shay Nehmad <shay.nehmad@guardicore.com>
2020-06-01 23:12:22 +0300

    Added get_commit_of utility function
    Also documentation

--- levels/checkers/common.sh
+++ levels/checkers/common.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Pass rejection reason as first parameter.
+# Example usage:
+# if [ ! -f runme.py ];
+#     then reject-solution "runme.py is missing.";
+# fi
 reject-solution () {
 	message=$1
 	echo "Solution rejected. Reason: "
@@ -7,6 +12,10 @@ reject-solution () {
 	exit 1
 }
 
+# Used to dump commit contents to a directory. Returns the path. 
+# Example usage: 
+# dump_dir=$(dump-commit-to-directory $new)
+# pushd $dump_dir
 dump-commit-to-directory () {
 	commit_hash=$1
 	temp_dir=$(mktemp -d)
@@ -14,7 +23,7 @@ dump-commit-to-directory () {
 	echo $temp_dir
 }
 
-# Override pushd and popd to be silent
+# Override pushd and popd to be silent, use like normal pushd and popd
 pushd () {
     command pushd "$@" > /dev/null
 }
@@ -47,4 +56,11 @@ print_seperator() {
 	echo
 	echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
 	echo
+}
+
+
+# Pass git pointer (branch, tag, commit)
+# Example usage: target_commit=$(get_commit_of steek-shabandar-taenifuge-tag)
+get_commit_of() {
+    echo $(git rev-list -n 1 $1)
 }

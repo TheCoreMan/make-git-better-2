500288775c8b38dd417f738e3c3a7231eb7ada41
Shay Nehmad <shay.nehmad@guardicore.com>
2020-06-01 23:15:44 +0300

    Changed player default editor to vim.

--- build/player_entrypoint.sh
+++ build/player_entrypoint.sh
@@ -16,6 +16,6 @@ cat ~/.ssh/id_rsa.pub >> /tmp/id_rsa.player.pub
 echo "Setting up zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-
 git config --global user.email "player@mrnice.dev"
 git config --global user.name "CTF player"
+git config --global core.editor vim

5c6d2bb3f66a9382d8bbd7f500a7d03f8c60b169
Shay Nehmad <shay.nehmad@guardicore.com>
2020-06-12 03:09:33 +0300

    Update checkers to autofail (shouldn't get to them)

--- levels/checkers/tag-1.sh
+++ levels/checkers/tag-1.sh
@@ -4,14 +4,8 @@ source $(dirname $0)/checkers-lib.sh
 
 read old new ref < /dev/stdin
 
+reject-solution "Shouldn't push in this level."
+
 # level info
 ## title tag-1 
 ## branch redamage-bundh-passerina -> That means the tag is redamage-bundh-passerina-tag
-
-echo "testing"
-echo "old $old"
-echo "new $new"
-echo "ref $ref"
-echo "finished"
-
-exit 1
--- levels/checkers/tag-2.sh
+++ levels/checkers/tag-2.sh
@@ -4,11 +4,7 @@ source $(dirname $0)/checkers-lib.sh
 
 read old new ref < /dev/stdin
 
-dump_dir=$(dump-commit-to-directory $new)
-
-pushd $dump_dir
-    # Check files here
-popd
+reject-solution "Shouldn't push in this level."
 
 # level info
 ## title tag-2 

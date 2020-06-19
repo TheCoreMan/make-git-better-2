7e00d21ea7038b0b6721ca1ffb6450f03ec55adc
Shay Nehmad <shay.nehmad@guardicore.com>
2020-06-20 00:41:14 +0300

    Added support for dumb terminals
    and a helper script to count current players on the server

--- build/player_zshrc.sh
+++ build/player_zshrc.sh
@@ -8,3 +8,14 @@ source /usr/share/doc/fzf/examples/key-bindings.zsh
 source /usr/share/doc/fzf/examples/completion.zsh
 
 alias python=python3
+
+# See https://twitter.com/_bsless/status/1274072996689436674
+if [[ "$TERM" == "dumb" ]]
+then
+  unsetopt zle
+  unsetopt prompt_cr
+  unsetopt prompt_subst
+  unfunction precmd
+  unfunction preexec
+  PS1='$ '
+fi

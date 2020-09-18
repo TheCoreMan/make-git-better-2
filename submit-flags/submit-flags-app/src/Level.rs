cd0b513c1951ee440b600f39b3e33742720ac2cc
Shay Nehmad <dude500@gmail.com>
2020-09-18 17:59:48 +0300

    WIP on the level component, still a few changes left

--- submit-flags/submit-flags-app/src/Level.rs
+++ submit-flags/submit-flags-app/src/Level.rs
@@ -1,37 +1,79 @@
+use log;
+
 use yew::prelude::{Component, ComponentLink, Properties, html, Html, ShouldRender};
+use yew::html::InputData;
 
 pub struct LevelComponent {
+    // The link enables us to interact (i.e. reqister callbacks and send messages) with the component itself. See https://yew.rs/docs/en/concepts/components/#create
     link: ComponentLink<Self>,
+    // The level's name. This is so the user knows which flag belongs where
     name: String,
+    // The flag itself. In the future this will become a hash so that the users can't get the flags using devtools.
     flag: String,
-    flagCorrect: bool,
+    // The user's guess for the flag, that they are typing
+    user_flag: String,
+    // Whether the correct flag has been entered.
+    flag_correct: bool,
 }
 
+// These are the messages (think "events") that can happen in this component.
 pub enum LevelMsg {
-    Clicked,
+    // This message indicates that it's time to check the user flag to see if it's the correct one.
+    CheckFlag,
+    // This message indicates that the user changed the flg they're guessing (when they're typing). Since we need to pass a value, this message has a parameter - see the `view` and `update` methods to see how this is used.
+    UserFlagChanged(String),
 }
 
+// See https://yew.rs/docs/en/concepts/components/properties/
+// The properties allow enable child and parent components to communicate with each other.
+// The parent of a level component is the page itself.
 #[derive(Clone, PartialEq, Properties)]
 pub struct LevelProps {
-    #[prop_or_default]
+    // This prop is the level's name. Passed from parent and won't change
     pub name: String,
+    // This prop is the level's flag. Passed from parent and won't change
     pub flag: String,
+    // This prop indicates whether the user's flag is correct. Not passed from parent, but rather used to communicate back to it from the level.
+    #[prop_or(false)]
+    pub flag_correct: bool,
 }
 
+// See https://yew.rs/docs/en/concepts/components/
+// `Component` is a Trait (see https://doc.rust-lang.org/book/ch10-02-traits.html), defined here: https://github.com/yewstack/yew/blob/master/yew/src/html/mod.rs#L30 
 impl Component for LevelComponent {
+    // Overriding properties since we have our own.
     type Properties = LevelProps;
+    // Overriding `Message` since we have our own messages.
     type Message = LevelMsg;
 
+    // See https://yew.rs/docs/en/concepts/components/#create
     fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        log::debug!("Creating level {} component", props.name.clone());
         Self {
             link: link,
+            // Pass the name from the parent component
             name: props.name,
+            // Pass the flag from the parent component
             flag: props.flag,
-            flagCorrect: false,
+            // The initial user flag is empty
+            user_flag: "".to_string(),
+            // This is passed from the parent component as well, but has a default value of `false`.
+            flag_correct: props.flag_correct,
         }
     }
 
-    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            LevelMsg::CheckFlag => {
+                // TODO - hash
+                self.flag_correct = self.user_flag == self.flag
+            }
+            LevelMsg::UserFlagChanged(value) => {
+                log::debug!("In level {}, User flag changed from {} to {}", self.name.clone(), self.user_flag.clone(), value.clone());
+                self.user_flag = value;
+                self.update(LevelMsg::CheckFlag);
+            }
+        }
         true
     }
 
@@ -43,11 +85,28 @@ impl Component for LevelComponent {
     }
 
     fn view(&self) -> Html {
+        let label_text = self.name.clone() + "'s flag goes here 🚩";
         html! {
             <>
-                <h1>{ "Level Component is here!" }</h1>
-                <p>{"Name: "}{self.name.clone()}{" | Flag: "}{self.flag.clone()}{" | Status: "}{get_correct_emoji(self.flagCorrect)}</p>
-                <button class="menu-btn">{"Check flag"}</button>
+                <span>
+                    <pre>
+                        {"DEBUG: I am a level component! Name: "}{self.name.clone()}
+                        {" | Flag: "}{self.flag.clone()}
+                        {" | Status: "}{get_correct_emoji(self.flag_correct)}
+                    </pre>
+                    <br/>
+                    <div>
+                        <div class="input-effect">
+                        <input class={ format!("effect-8 effect-10-{}", if self.flag_correct { "good" } else { "bad" }) } type="text" placeholder={label_text.clone()} oninput=self.link.callback(|e: InputData| LevelMsg::UserFlagChanged(e.value)) />
+                            <span class="focus-bg"></span>    
+                            <span class="focus-border">
+                                <i></i>
+                            </span>
+                        </div>
+                        // TODO - delete button
+                        <button id="check-flag-btn" onclick=self.link.callback(|_| LevelMsg::CheckFlag)><span>{"Check flag"}</span></button>
+                    </div>
+                </span>
             </>
         }
     }
--- submit-flags/submit-flags-app/src/lib.rs
+++ submit-flags/submit-flags-app/src/lib.rs
@@ -1,8 +1,12 @@
-mod Level;
+// See https://github.com/yewstack/yew/issues/97
+#![recursion_limit="256"]
+
+mod level;
 
 use wasm_bindgen::prelude::{wasm_bindgen};
 use yew::prelude::{Component, ComponentLink, ShouldRender, Html, html, App};
-use Level::LevelComponent;
+use level::LevelComponent;
+use wasm_logger;
 
 struct Model {
     link: ComponentLink<Self>,
@@ -41,8 +45,8 @@ impl Component for Model {
         html! {
             <>
                 <main class="site-main section-inner thin animated fadeIn">
-                    <h1>{ "Make Git Better CTF - Submit Flags" }</h1>
-                    <div class="content">
+                    <h1 id="home-title">{ "Make Git Better CTF - Submit Flags" }</h1>
+                    <div id="single-level-checker" class="content">
                         <LevelComponent name="levelname" flag="levelflag"/>
                     </div>
                 </main>
@@ -53,5 +57,6 @@ impl Component for Model {
 
 #[wasm_bindgen(start)]
 pub fn run_app() {
+    wasm_logger::init(wasm_logger::Config::default());
     App::<Model>::new().mount_to_body();
 }
